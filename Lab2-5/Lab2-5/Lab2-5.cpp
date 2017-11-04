2dad35350558dc918eb954fe46236bbcd0373d1d
gera-vovano <gerasimenko_99@list.ru>
2017-11-04 19:55:21 +0300

    Доработал, надеюсь, замечаний не будет...

--- Lab2-5/Lab2-5/Lab2-5.cpp
+++ Lab2-5/Lab2-5/Lab2-5.cpp
@@ -1,64 +1,70 @@
-﻿/*
-Герасименко Владимир Александрович
-Лабораторная работа №2.
-Вариант №5
-Задание: Написать программу, которая вычисляет произведение всех отрицательных элементов массива.
-Массив и его длина вводятся пользователем.
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <locale.h>
-#include <conio.h>
-#define SIZE 100
-int CheckArray(int size);
-int CheckMass(int size);
+﻿		/*
+		Герасименко Владимир Александрович
+		Лабораторная работа №2.
+		Вариант №5
+		Задание: Написать программу, которая вычисляет произведение всех отрицательных элементов массива.
+		Массив и его длина вводятся пользователем.
+		*/
+
+	#define _CRT_SECURE_NO_WARNINGS
+	#define CONST 100
+	#include <stdio.h>
+	#include <locale.h>
+	#include <conio.h>
+	
+	int CheckArray(int size);
+	int CheckMass(int size,int i);
+	int CheckResult(int result);
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 	int  size = 0;
-	printf("Введите размерность массива не более чем %d", SIZE);
-	printf("\n");
+	printf("Введите размерность массива не более чем %d \n", CONST);
 	scanf("%d", &size);
 	CheckArray(size);
 }
-int CheckMass(int size)
+int CheckMass(int size,int i)
 {
-	int result, i;
+	int result;
 	float A;
-
 	result = 0;
 	while (result == 0)
 	{
-		printf("Введите элемент массива ");
+		printf("Введите элемент массива A[%d]: ", i );
 		result = scanf("%f", &A);
-		while (getchar() != '\n')
-			if (result == 0)
-			{
-				printf("Введите входные данные в виде числа! ");
-				break;
-			}
+		while (getchar() != '\n');
+		CheckResult(result);
 	}
 	return (A);
 }
+int CheckResult(int result)
+{
+	if (result == 0)
+	{
+		printf("Введите входные данные в виде числа! ");
+	}
+	return 0;
+}
 int CheckArray(int size)
 {
-	int i = 0, result = 0;
-	float  multiplication = 1, Array[100];
-	if (size <= SIZE)
+	int i = 0;
+	float  multiplication = 1, Array[CONST] = {0};
+	if (size <= CONST)
 	{
-
 		for (i = 0; i < size; i++)
 		{
-			Array[i] = CheckMass(size);
+			Array[i] = CheckMass(size,i);
 			if (Array[i] < 0)
 			{
 				multiplication *= Array[i];
 			}
 		}
-
 	}
-
+	else 
+	{
+		printf("Превышена размерность массива");
+	}
 	if (multiplication == 1)
 	{
 		printf("решения нет");
